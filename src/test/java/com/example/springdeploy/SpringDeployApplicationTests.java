85e62a80490d22d7c9786e37a934265c9c854e95
Pedro Serra <pedroserra86@gmail.com>
2022-11-23 22:19:24 -0300

    spring heroku app config

--- src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
+++ src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.springdeploy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringDeployApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.getenv().forEach(  //imprimimos las variables de entorno Ej username
+				(key, value) -> System.out.println(key + " " + value)
+		);
 	}
 
 }
